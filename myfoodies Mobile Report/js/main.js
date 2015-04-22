606860ddf1717e1d6e77fea51d08f10854f86f27
Chi Man Yung <michael.yung@everyware.com.hk>
2015-04-22 11:38:28 +0000

    Changes made by Chi Man Yung (michael.yung@everyware.com.hk).

--- myfoodies Mobile Report/js/main.js
+++ myfoodies Mobile Report/js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     
     $("#navbarcontent").append('<li><a href="dashboard.html" id="menu1"></a></li>');
     $("#navbarcontent").append('<li><a href="hourly.html" id="menu8"></a></li>');
-    $("#navbarcontent").append('<li><a href="#" id="menu7"></a></li>');
-    $("#navbarcontent").append('<li><a href="#" id="menu10"></a></li>');
+    $("#navbarcontent").append('<li><a href="byitem.html" id="menu7"></a></li>');
+    $("#navbarcontent").append('<li><a href="summary.html" id="menu10"></a></li>');
     $("#navbarcontent").append('<li><a href="#" id="menu6"></a></li>');
     $("#navbarcontent").append('<li><a href="#" id="menu2"></a></li>');
     $("#navbarcontent").append('<li><a href="#" id="menu3"></a></li>');
